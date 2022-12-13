94b4d77ed172cf0fe556c0c2ef8265e2e3b937ff
e232714 <e232714@sbl.ch>
2022-12-13 19:05:31 +0100

    bouncehinzugefügt

--- script.js
+++ script.js
@@ -7,7 +7,14 @@ function start() {
 
 function update() {
     dy = dy + 0.05
+
+
     let cy = parseFloat(circle.getAttribute("cy"))
+
+    if (cy > 100) {
+        dy = dy * (-1)
+    }
+
     cy = cy + dy
     circle.setAttribute("cy", cy)
 
